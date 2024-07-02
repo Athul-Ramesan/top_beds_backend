529dfd90d27a665b23226b34379cba706369ab91
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-07-02 19:37:54 +0530

    feat: chatgateway initiated

---
+++ chat/src/chat/chat.gateway.ts
@@ -0,0 +1,32 @@
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer }  from '@nestjs/websockets'
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway(8080,{})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
+   @WebSocketServer()
+   server:Server
+
+   handleConnection(client: Socket, ...args: any[]) {
+       console.log('New user connected', client.id)
+
+       this.server.emit('user-joined', {
+        message:`New user joined the chat:${client.id}`
+       })
+   }
+   handleDisconnect(client: Socket) {
+       console.log('User disconnected', client.id)
+
+       this.server.emit('user-left',{
+        message:`User left the chat:${client.id}`
+       })
+   }
+
+   @SubscribeMessage('message')
+   handleMessage(client:Socket, message:any): void {
+       console.log("🚀 ~ ChatGateway ~ handleMessage ~ message:", message)
+       
+    //    this.server.emit('message',message)
+    client.emit('reply', 'This is reply')
+    this.server.emit('reply' ,message)
+   }
+}
