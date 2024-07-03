f08ea117c2ed62cb0833d5dc26ee99560bd2abe4
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-07-03 11:34:19 +0530

    fix: cors origin fixed

--- chat/src/main.ts
+++ chat/src/main.ts
@@ -7,6 +7,12 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
+  const CLIENT_URL = configService.get<string>('CLIENT_URL')
+  app.enableCors({
+    origin: CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
   const PORT = configService.get<number>('PORT')
   await app.listen(PORT);
 }
