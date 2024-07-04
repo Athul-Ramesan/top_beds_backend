4c3390a1a5271cda876a5c056e34a4860ab50f6a
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-07-04 18:32:49 +0530

    refactor: chat service chat created message created

--- chat/src/chat/chat.controller.ts
+++ chat/src/chat/chat.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { CreateMessageDto } from './dto/createMessageDto';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+
+    constructor(private readonly chatService: ChatService) { }
+    @Post(':senderId/:receiverId')
+    async sendMessage(
+        @Param('senderId') senderId: string,
+        @Param('receiverId') receiverId: string,
+        @Body() createMessageDto: CreateMessageDto,) {
+        console.log("🚀 ~ ChatController ~ receiverId:", receiverId)
+        console.log("🚀 ~ ChatController ~ senderId:", senderId)
+        console.log("🚀 ~ ChatController ~ @Body ~ createMessageDto:", createMessageDto)
+        const createChatDto = {
+            participants: [senderId, receiverId],
+        }
+        const chat = await this.chatService.create(createChatDto);
+        const newMessageDto = {
+            ...createMessageDto,
+            senderId,
+            receiverId,
+          };
+        const newMessage = await this.chatService.createMessage(newMessageDto);
+        return newMessage;
+    }
+}
--- chat/src/chat/chat.service.ts
+++ chat/src/chat/chat.service.ts
@@ -3,13 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Chat, ChatDocument } from './schema/chat.model';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schema/user.model';
+import { Message, MessageDocument } from './schema/message.model';
+import { CreateMessageDto } from './dto/createMessageDto';
 
 @Injectable()
 export class ChatService {
 
     constructor(@InjectModel(Chat.name) private chatModel: Model<ChatDocument>,
 
-    @InjectModel(User.name) private userModel: Model<UserDocument>,){}
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Message.name) private messageModel: Model<MessageDocument>,){}
 
     async create(createChatDto:any):Promise<Chat>{
         const createdChat = new this.chatModel(createChatDto)
@@ -21,5 +24,9 @@ export class ChatService {
     }
     async getSenderDetails (senderId:string):Promise<User>{
         return this.userModel.findOne({_id: senderId})
+    }
+
+    async createMessage (createMessageDto:CreateMessageDto):Promise<Message>{
+        return this.messageModel.create(createMessageDto)
     }
 }
