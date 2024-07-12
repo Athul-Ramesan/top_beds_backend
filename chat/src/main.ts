8dacd1b048e5a309c0a16bcba166517148468a7e
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-07-12 11:38:10 +0530

    refactor: main file updated

--- chat/src/main.ts
+++ chat/src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   const CLIENT_URL = configService.get<string>('CLIENT_URL')
