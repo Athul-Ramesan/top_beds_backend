2bd08d1233ce70585918d70f036d30e83bad3555
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-10-16 21:12:52 +0530

    kafka configured to confluentCloud

--- booking/src/app.module.ts
+++ booking/src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 import { BookingModule } from './booking/booking.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_CONNECTION } from './app.properties';
 import { KafkaModule } from './kafka/kafka.module';
-import { ConfigModule } from '@nestjs/config';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { CronModule } from './cron/cron.module';
 import { ReviewModule } from './review/review.module';
-import { DashboardController } from './dashboard/dashboard.controller';
 import { DashboardModule } from './dashboard/dashboard.module';
-import config from './config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule,ConfigService } from '@nestjs/config';
 
 
 
 @Module({
   imports: [ 
-    MongooseModule.forRoot(config.urls.mongoUrI),
+    // MongooseModule.forRoot(config.urls.mongoUrI),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_CONNECTION'),
+      }),
+      inject: [ConfigService],
+    }),
     ScheduleModule.forRoot(),
     BookingModule,
     KafkaModule,
